21948f89163a9ecd9918854c5788dada1e95e95c
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-23 17:35:47 +0530

    DateTime

--- Program.cs
+++ Program.cs
@@ -30,6 +30,7 @@ namespace BethanysPieShopHRM
             bool a; // Default assigned value = false
             int b;  // Default assigned value = 0
 
+            Console.WriteLine("Pleas Enter Any Number :");
             string input = Console.ReadLine();
             int number = Convert.ToInt32(input);
             Console.WriteLine($"You entered {number}");
@@ -42,7 +43,30 @@ namespace BethanysPieShopHRM
             bool isDigit = char.IsDigit(userSelection);
             bool isLetter = char.IsLetter(userSelection);
 
+            DateTime hireDate = new DateTime(2022, 03, 28, 14, 30, 0);
+            Console.WriteLine($"Hired Date is {hireDate}");
 
+            DateTime exitDate = new DateTime(2022, 12, 12);
+            Console.WriteLine($"Leaving Date is {exitDate}");
+
+            // DateTime invalidDate = new DateTime(2022, 25, 25); // Exception : Un-representable DateTime
+
+            DateTime startDate = hireDate.AddDays(15);
+
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine(currentDate);
+
+            bool areWeInDst = currentDate.IsDaylightSavingTime();
+            Console.WriteLine($"Are We In Day light Saving Time {areWeInDst}");
+
+            DateTime startHour = DateTime.Now;
+            Console.WriteLine(startHour.ToLongDateString());
+
+            TimeSpan workTime = new TimeSpan(8, 20, 12);
+            DateTime endHour = startHour.Add(workTime);
+            Console.WriteLine(endHour.ToShortDateString());
+
+            Console.WriteLine($"Work end hour is {endHour}");
         }
     }
 }
