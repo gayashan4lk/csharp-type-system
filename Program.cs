14acb0c56bc6c73fa56edb624cde743feaa1e92b
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-23 17:02:59 +0530

    Operators and default values

--- Program.cs
+++ Program.cs
@@ -10,9 +10,25 @@ namespace BethanysPieShopHRM
             int months = 12, bonus = 1000;
             bool isActive = true;
             double rating = 99.9;
-            byte numberOfEmployees = 120;
+            double ratePerHour = 12.34;
+            int numberOfHoursWorked = 165;
 
-            Console.ReadLine();
+            double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
+
+            Console.WriteLine(currentMonthWage);
+
+            ratePerHour += 3; // ratePerHour = ratePerHour + 3;
+
+            if (currentMonthWage > 2000)
+            {
+                Console.WriteLine("Top paid employee");
+            }
+
+            int numberOfEmployees = 15;
+            numberOfEmployees--;
+
+            bool a; // Default assigned value = false
+            int b;  // Default assigned value = 0
         }
     }
 }
