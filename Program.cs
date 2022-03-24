28101c27b006dc4d5c1741f3fbf53f371879e747
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-24 08:50:49 +0530

    passing parameters by reference

--- Program.cs
+++ Program.cs
@@ -7,6 +7,33 @@ namespace BethanysPieShopHRM
     {
         static void Main(string[] args)
         {
+            int numberOfMonths = 12;
+            int bonus = 5000;
+            Console.WriteLine(CalcSal(2500, numberOfMonths, bonus));
+            Console.WriteLine(bonus);
+            Console.WriteLine(CalcSal(2456.45543, ref bonus));
+            Console.WriteLine(bonus);
+        }
+        
+        public static int CalcSal(int monthlySal, int numberOfMonths, int bonus)
+        {
+            if(monthlySal < 2000)
+            {
+                bonus *= 2;
+                Console.WriteLine("Bonus doubled.");
+            }
+            return monthlySal * numberOfMonths + bonus;
+        }
+
+        public static double CalcSal(double monthlySal, ref int bonus)
+        {
+            if(monthlySal < 3000)
+            {
+                bonus *= 3;
+                Console.WriteLine("Bonus trippled");
+            }
+            double result = monthlySal *  bonus;
+            return result;
         }
         
     }
